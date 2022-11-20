8a32a58ce650636978670225af6a06084252491c
Hector <66312947+Hectorfr@users.noreply.github.com>
2022-11-21 00:43:13 +0100

    Not finished

--- ex32.java
+++ ex32.java
@@ -10,125 +10,130 @@ public class ex32 {
            String W, S, out = "";
            int N, spaces = 0;
            double parameter2 = 0, middle;
-        
-           W = s.nextLine();
-           N = Integer.parseInt(s.nextLine());
-           S = s.nextLine();
+           while (s.hasNext()){
+
+           
+           W = s.next();
+           N = Integer.parseInt(s.next());
+           S = s.next();
     
-           switch (W){
-                case "square":
-                    for (int i = 0 ; i < N ; i++) {
-                        System.out.printf("\n");
-                        for (int p = 0 ; p < (N*2) ; p++) {
-                            System.out.printf(S);
-                        }
-                    }
-                    break;
-                
-                case "triangle":
-                    for (int i = 0 ; i < N ; i++) {
-                        System.out.printf("\n");
-                        spaces = N+i;
-                        out = "";
-                        for (int p = 0; p < 2*i+1; p++) {
-                            out += S;
-                            if  (p == 2*i) System.out.printf("%" + spaces +"s",out);
-                        }
-                    }
-                    break;
+                switch (W){
+                        case "square":
+                            for (int i = 0 ; i < N ; i++) {
+                                for (int p = 0 ; p < (N*2) ; p++) {
+                                    System.out.printf(S);
+                                }
+                                System.out.printf("\n");
+                            }
+                        break;
                     
-                case "down-arrow":
-                    for (int i = N ; i > 0; i--) {
-                        System.out.printf("\n");
-                        spaces = N+i-1;
-                        out = "";
-                        for (int p = 0; p < 2*i-1; p++) {
-                            out += S;
-                            if  (p == 2*i-2) System.out.printf("%" + spaces +"s",out);
-                        }
-                    }
-                    break;
-
-                case "left-arrow":
-                    middle = (2*N -1)/2.0;
-                    for (int i = 0 ; i < (2*N -1); i++) {
-                        System.out.printf("\n");
-                        spaces = 2*N-1;
-                        out = "";
-                        if (i <= middle){
-                            for (int p = 0; p < 2*i+1; p++) {
-                              out += S;
-                                if  (p == 2*i) System.out.printf("%" + spaces +"s",out);
-                            }
-                        }else if (i > middle){
-                            parameter2 = (middle + (middle-1) - ((i - middle)*2.0));
-                            for (int p = 0; p < parameter2; p++) {
-                                out += S;
-                                if  (p == parameter2-1) System.out.printf("%" + spaces +"s",out);
-                            }
-                        }
-                    }   
-                    break;
-
-                case "right-arrow":
-                    middle = (2*N -1)/2.0;
-                    for (int i = 0 ; i < (2*N -1); i++) {
-                        System.out.printf("\n");
-                        spaces = 2*N-1;
-                        out = "";
-                        if (i <= middle){
-                            for (int p = 0; p < 2*i+1; p++) {
-                                out += S;
-                                if  (p == 2*i) System.out.printf("%-" + spaces +"s",out);
-                            }
-                        }else if (i > middle){
-                            parameter2 = (middle + (middle-1) - ((i - middle)*2.0));
-                            for (int p = 0; p < parameter2; p++) {
-                                out += S;
-                                if  (p == parameter2-1) System.out.printf("%-" + spaces +"s",out);
-                            }
-                        }
-                    }
-                    break;
-
-
-                case "diamond":
-                    middle = (2*N - 1)/2.0;
-                    for (int i = 0 ; i < middle ; i++) {
-                        System.out.printf("\n");
-                        out = "";
-                        spaces = (N+i);
+                    case "triangle":
+                        for (int i = 0 ; i < N ; i++) {
+                            spaces = N+i;
+                            out = "";
                             for (int p = 0; p < 2*i+1; p++) {
                                 out += S;
                                 if  (p == 2*i) System.out.printf("%" + spaces +"s",out);
                             }
-                    }
-                    
-                    for (int t = (int)middle ; t > 0 ; t--){
-                        System.out.printf("\n");
-                        out = "";
-                        spaces = N+t-1;
-                        for (int p = 0; p < 2*t-1; p++) {
-                            out += S;
-                            if  (p == 2*t-2) System.out.printf("%" + spaces +"s",out);
+                            System.out.printf("\n");
                         }
-                    }
-                    break;
+                        break;
+                        
+                    case "down-arrow":
+                        for (int i = N ; i > 0; i--) {
+                            spaces = N+i-1;
+                            out = "";
+                            for (int p = 0; p < 2*i-1; p++) {
+                                out += S;
+                                if  (p == 2*i-2) System.out.printf("%" + spaces +"s",out);
+                            }
+                            System.out.printf("\n");
+                        }
+                        break;
 
-                case "trapecious":
-                    for (int i = 1 ; i < N+1 ; i++) {
-                        System.out.printf("\n");
-                        spaces = 2*N+i-2;
-                        out = "";
-                        for (int p = 0; p < 2*i+1; p++) {
-                            out += S;
-                            if  (p == 2*i) System.out.printf("%" + spaces +"s",out);
+                    case "left-arrow":
+                        middle = (2*N -1)/2.0;
+                        for (int i = 0 ; i < (2*N -1); i++) {
+                            spaces = 2*N-1;
+                            out = "";
+                            if (i <= middle){
+                                for (int p = 0; p < 2*i+1; p++) {
+                                out += S;
+                                    if  (p == 2*i) System.out.printf("%" + spaces +"s",out);
+                                }
+                            }else if (i > middle){
+                                parameter2 = (middle + (middle-1) - ((i - middle)*2.0));
+                                for (int p = 0; p < parameter2; p++) {
+                                    out += S;
+                                    if  (p == parameter2-1) System.out.printf("%" + spaces +"s",out);
+                                }
+                            }
+                            System.out.printf("\n");
+                        }   
+                        break;
+
+                    case "right-arrow":
+                        middle = (2*N -1)/2.0;
+                        for (int i = 0 ; i < (2*N -1); i++) {
+                            spaces = 2*N-1;
+                            out = "";
+                            if (i <= middle){
+                                for (int p = 0; p < 2*i+1; p++) {
+                                    out += S;
+                                    if  (p == 2*i) System.out.printf("%-" + spaces +"s",out);
+                                }
+                            }else if (i > middle){
+                                parameter2 = (middle + (middle-1) - ((i - middle)*2.0));
+                                for (int p = 0; p < parameter2; p++) {
+                                    out += S;
+                                    if  (p == parameter2-1) System.out.printf("%-" + spaces +"s",out);
+                                }
+                            }
+                            System.out.printf("\n");
                         }
-                    }
-                    break;
-                default: 
-                    System.out.printf("Wrong selection!");
-           }
+                        break;
+
+
+                    case "diamond":
+                        middle = (2*N - 1)/2.0;
+                        for (int i = 0 ; i < middle ; i++) {
+                            out = "";
+                            spaces = (N+i);
+                                for (int p = 0; p < 2*i+1; p++) {
+                                    out += S;
+                                    if  (p == 2*i) System.out.printf("%" + spaces +"s",out);
+                                }
+                            System.out.printf("\n");
+                        }
+                        
+                        for (int t = (int)middle ; t > 0 ; t--){
+                            out = "";
+                            spaces = N+t-1;
+                            for (int p = 0; p < 2*t-1; p++) {
+                                out += S;
+                                if  (p == 2*t-2) System.out.printf("%" + spaces +"s",out);
+                            }
+                            System.out.printf("\n");
+                        }
+                        break;
+
+                    case "trapecious":
+                        for (int i = 1 ; i < N+1 ; i++) {
+                            spaces = 2*N+i-2;
+                            out = "";
+                            for (int p = 0; p < 2*i+1; p++) {
+                                out += S;
+                                if  (p == 2*i) System.out.printf("%" + spaces +"s",out);
+                            }
+                            System.out.printf("\n");
+                        }
+                        break;
+
+                    default: 
+                        System.out.printf("Wrong selection!");
+                }
+                System.out.printf("\n");
+            }
         }
 }
 
