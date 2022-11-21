ab9f516ef14cb5498b336e96919c594169e1f253
Hector <66312947+Hectorfr@users.noreply.github.com>
2022-11-21 18:17:27 +0100

    Add files via upload

---
+++ ex36.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+
+
+public class ex36 {
+    public static void main(String [] args){
+        Scanner s = new Scanner(System.in).useLocale(Locale.US); 
+
+        long N, count = 0, i = 2, sq;
+        boolean b = false;
+
+        while (s.hasNextLong()){
+
+            N = s.nextLong();
+
+            // Reset values to default for every iteration
+            i = 2;
+            b = false;
+
+            sq = (int)Math.sqrt(N);
+
+            while (i <= sq){
+                if (N % i == 0){b = true;}
+                i++;
+            }
+            if (b == false) count++;
+        }
+        if (count==0){
+            System.out.print("No prime numbers were found!\n");
+        }else if (count==1){
+            System.out.print("Only one prime number was found!\n");
+        }else{
+            System.out.printf("%l prime numbers were found!\n", count);
+        }
+   }
+}
