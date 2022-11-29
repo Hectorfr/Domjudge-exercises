2168d5344522885dd6d9ab7c73c9439f96ba99d6
Hector <66312947+Hectorfr@users.noreply.github.com>
2022-11-29 22:08:02 +0100

    Add files via upload

--- ex39.java
+++ ex39.java
@@ -1,37 +1,80 @@
+package DomJudge.Domjudge;
 import java.util.*;
 
 public class ex39 {
     public static void main(String [] args){
-        Scanner s = new Scanner(System.in).useLocale(Locale.US);
+        Scanner s = new Scanner(System.in).useLocale(Locale.US);       
+        char Z;
+        String line = "", trash = "firstTime";
+        float influence = 0, force, a = 0, b = 0, c = 0, d, e, f, distance;
+
+
+        while(s.hasNextLine()){
+            if (s.hasNext() == false) break;
+            line = s.next();
+            Z = line.charAt(0);
         
-        float x, y, influence = 0, A, B, C;
-        int risk = 0;
+            // String numeros[] = line.split(":");
 
-        A = s.nextFloat();
-        B = s.nextFloat();
-        C = s.nextFloat();
+            if (Z == 't'){
+                if (trash != "firstTime") {
+                    System.out.printf("mortal at (%.2f,%.2f,%.2f)", a,b,c);
+                    if (influence > 0){
+                        System.out.printf(" is protected\n");
 
-        while(s.hasNextFloat()){
-            x = s.nextFloat();
-            y = s.nextFloat();
-            influence = 
-            equation = A*x+B*y+C;
+                    }else if (influence < 0){
+                        System.out.printf(" is in danger\n");
+
+                    }else{
+                        System.out.printf(" is in no man's land\n");
+                    }
+                }
+                trash = s.next();
+                a = s.nextFloat();
+                b = s.nextFloat();
+                c = s.nextFloat();
+                influence = 0;
+                continue;
+            }//Float.parseFloat(numeros[1])
             
-            if (equation <= 1 && equation >= -1){
-                risk = 0;
-            }else if (equation > 1){
-                risk = 1;
-            }else if (equation < -1){
-                risk = -1;
-            }
-            switch (risk){
-                case 1: System.out.printf("mortal at (%.2f,%.2f,%.2f) is protected", A,B,C);
-                break;
-                case 0: System.out.printf("mortal at (%.2f,%.2f,%.2f) is in no man\'s land", A,B,C);
-                break;
-                case -1: System.out.printf("mortal at (%.2f,%.2f,%.2f) is in danger", A,B,C);
-                break;
+            //Ruben helped me
+            trash = s.next();
+            trash = s.next();
+            trash = s.next();
+            if (Z == 's') trash = s.next();
+            
+            d = s.nextFloat();
+            e = s.nextFloat();
+            f = s.nextFloat();
+
+            force = s.nextFloat();
+            
+            float x = d-a;
+            float y = e-b;
+            float z = f-c;
+
+            distance = (float) Math.sqrt(x*x + y*y + z*z);
+
+            // System.out.println(line);
+            if (Z == 'j'){
+                if (distance < force){
+                    influence = influence + force; 
+                }
+            }else if (Z == 's'){
+                if (distance < force){
+                    influence = influence - force; 
+                }
             }
         }
+        System.out.printf("mortal at (%.2f,%.2f,%.2f)", a,b,c);
+            if (influence > 0){
+                System.out.printf(" is protected\n");
+
+            }else if (influence < 0){
+                System.out.printf(" is in danger\n");
+
+            }else{
+                System.out.printf(" is in no man's land\n");
+            }
     }
 }
